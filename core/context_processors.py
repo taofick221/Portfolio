702c834721f70b89778d5e2d07342f9ab20d9bbb
Taofick <mdtaofick45418@gmail.com>
2026-09-02 21:02:45 +0600

    Fix sidebar spacing

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,9 +1,10 @@
-from .models import SiteSettings
 from portfolio.models import Profile, SocialLink
+
 
 def site_context(request):
     return {
-        "site_settings": SiteSettings.objects.first(),
         "profile": Profile.objects.first(),
-        "social_links": SocialLink.objects.filter(is_active=True).order_by("order"),
+        "social_links": SocialLink.objects.filter(
+            is_active=True
+        ).order_by("order", "id"),
     }
